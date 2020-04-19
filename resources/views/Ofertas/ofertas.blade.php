5da061618646e05c8d506871f85f07e19b61a5f6
Noel Mora <noel15192@gmail.com>
2020-04-19 16:27:13 -0500

    Carrusel

--- resources/views/Ofertas/ofertas.blade.php
+++ resources/views/Ofertas/ofertas.blade.php
@@ -20,7 +20,7 @@
     <hr>
 
     <div class="agregar-oferta">
-      <p> 
+      <p>
           <a class="btn btn-warning btn-block" href="{{ route('oferta.create') }}"> Publicar Oferta </a>
       </p>
     </div>
@@ -36,17 +36,19 @@
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
       </ol>
+      <!-- Imagenes del Carrusel -->
       <div class="carousel-inner" role="listbox">
         <div class="carousel-item active">
-          <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide">
+          <img class="d-block img-fluid" src="http://lorempixel.com/900/350/technics/2" alt="First slide">
         </div>
         <div class="carousel-item">
-          <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">
+          <img class="d-block img-fluid" src="http://lorempixel.com/900/350/technics/5" alt="Second slide">
         </div>
         <div class="carousel-item">
-          <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide">
+          <img class="d-block img-fluid" src="http://lorempixel.com/900/350/technics/1" alt="Third slide">
         </div>
       </div>
+      <!-- Flechas Atras - Adelante -->
       <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
@@ -76,7 +78,7 @@
 
       @endforeach
 
-      
+
 
     </div>
     <!-- /.row -->
